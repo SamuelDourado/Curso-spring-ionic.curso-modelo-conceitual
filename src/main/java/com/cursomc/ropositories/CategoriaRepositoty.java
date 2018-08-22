1faece54c1426b38975c9b4552bfed7eea618e90
unknown <sd7038@gmail.com>
2018-08-22 16:31:30 -0300

    Imterface CategoriaRepository criada

---
+++ src/main/java/com/cursomc/ropositories/CategoriaRepositoty.java
@@ -0,0 +1,9 @@
+package com.cursomc.ropositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursomc.domain.Categoria;
+
+public interface CategoriaRepositoty extends JpaRepository<Categoria, Integer>{
+
+}
