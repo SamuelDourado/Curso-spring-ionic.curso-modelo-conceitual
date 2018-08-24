abb3ed50f459078af2bf05712f15dca9ab7721a3
unknown <sd7038@gmail.com>
2018-08-24 13:25:01 -0300

    Adicionado comandos para inicializar dados no banco de dados

--- src/main/java/com/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/cursomc/resource/CategoriaResource.java
@@ -3,24 +3,25 @@ package com.cursomc.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.servicee.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	@Autowired
+	private CategoriaService service;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria categoria1 = new Categoria(1, "Informática");
-		Categoria categoria2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.add(categoria1);
-		categorias.add(categoria2);
-		return categorias;
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Categoria obj = this.service.busca(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
