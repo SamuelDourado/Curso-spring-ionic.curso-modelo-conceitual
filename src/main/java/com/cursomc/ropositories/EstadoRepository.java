2537d2a04059fb404ceecf3bcac6189e7b61c5d0
unknown <sd7038@gmail.com>
2018-08-27 15:39:47 -0300

    Classes do domínio Cidade e Estado criadas

---
+++ src/main/java/com/cursomc/ropositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.cursomc.ropositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
